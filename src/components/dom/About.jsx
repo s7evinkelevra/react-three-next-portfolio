bb18876de6efcd11dd1cd932199e3dd085125622
s7evinkelevra <kelevra.1337@gmail.com>
2022-10-05 19:58:09 +0200

    create about comp

---
+++ src/components/dom/About.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Image from 'next/image';
+import profilePic from 'public/img/jan_comp.jpg'
+
+
+import styles from './About.module.css'
+
+const About = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.box}>
+        <div className={styles.text}>
+          <h1>JAAA Dance you fools!</h1>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+        </div>
+      </div>
+
+      <div className={styles.content}>
+        <div className={styles.image_container}>
+          <Image src={profilePic} />
+        </div>
+      </div>
+    </div>
+
+  );
+};
+
+export default About;
