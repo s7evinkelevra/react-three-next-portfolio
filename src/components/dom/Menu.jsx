676b361ee76b2c40ceebb890f5a49026e341f6de
s7evinkelevra <kelevra.1337@gmail.com>
2022-10-05 19:58:21 +0200

    move menu into own comp

---
+++ src/components/dom/Menu.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Link from "next/link"
+
+import styles from './Menu.module.css'
+
+
+
+const MenuLink = ({ href, children }) => (
+  <Link href={href}>
+    <a className={styles.menu_link}>{children}</a>
+  </Link>
+)
+
+
+const Menu = ({ width }) => {
+  return (
+    <div className={styles.fixed_menu_container} style={{ width }}>
+      <div className={styles.fixed_menu}>
+        <MenuLink href="/">Home</MenuLink>
+        <MenuLink href="/portfolio">Portfolio</MenuLink>
+        <MenuLink href="/about">About Me</MenuLink>
+
+      </div>
+    </div>
+
+  );
+};
+
+export default Menu;
