9564ab3d0b7b3d26490cf5c9395a30b32e33b9e5
s7evinkelevra <kelevra.1337@gmail.com>
2022-10-06 02:18:29 +0200

    generate static props and pass them to the comps

--- src/pages/portfolio/index.jsx
+++ src/pages/portfolio/index.jsx
@@ -2,14 +2,27 @@ import React from 'react';
 import dynamic from 'next/dynamic'
 import Menu from '@/components/dom/Menu';
 
+import { serialize } from 'next-mdx-remote/serialize'
+import { MDXRemote } from 'next-mdx-remote'
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+
+import Portfolio from '@/components/dom/Portfolio';
+import { useRouter } from 'next/router';
+
 
 // dom components goes here
-const Page = (props) => {
+const Page = ({ projects }) => {
+  const { query } = useRouter();
+  const currentProjectSlug = query?.project || null
 
+  console.log({ projects })
+  console.log(currentProjectSlug)
 
   return (
     <>
-      portfolio lols
+      <Portfolio projects={projects} currentProjectSlug={currentProjectSlug} />
       <Menu width={"600px"} />
     </>
   )
@@ -27,9 +40,41 @@ Page.r3f = (props) => {
 export default Page
 
 export async function getStaticProps() {
+  const files = fs.readdirSync(path.join('projects'));
+
+  const projects = [];
+
+  for (var filename of files) {
+    const markdownWithMeta = fs.readFileSync(path.join('projects', filename), 'utf-8');
+    const { data: frontMatter, content } = matter(markdownWithMeta);
+
+    const mdxSource = await serialize(content);
+
+    projects.push({
+      ...frontMatter,
+      slug: filename.split('.')[0],
+      mdxSource
+    })
+  }
+
+
+  /*   const projects = files.map(async (filename) => {
+      const markdownWithMeta = fs.readFileSync(path.join('projects', filename), 'utf-8');
+      const { data: frontMatter, content } = matter(markdownWithMeta);
+  
+      const mdxSource = await serialize(content);
+  
+      return {
+        ...frontMatter,
+        slug: filename.split('.')[0],
+        mdxSource
+      }
+    }) */
+
   return {
     props: {
       title: 'Portfolio',
+      projects
     },
   }
 }
