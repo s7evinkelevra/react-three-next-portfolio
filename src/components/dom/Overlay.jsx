59e5084c23d20859469c840c755c5a09f0030321
s7evinkelevra <kelevra.1337@gmail.com>
2022-10-07 22:38:50 +0200

    update content

--- src/components/dom/Overlay.jsx
+++ src/components/dom/Overlay.jsx
@@ -36,22 +36,23 @@ const Overlay = forwardRef(({ scroll }, ref) => {
 
       <Section height="100vh">
         <h1>Hi, I&apos;m Jan!</h1>
-        I&apos;m a developer mostly interested in web/react, with &gt;&nbsp;30 professional projects completed since 2018.
-        Scroll to take a quick tour of what i do, or see the <Link href="/portfolio"><a>portfolio</a></Link> for a comprehensive overview.
+        I&apos;m a developer mostly interested in creating for the web, preferably with react. Together with my brother, i&apos;ve build a business on that, with &gt;&nbsp;30 professional projects completed since we started in 2018.
+        <br />Scroll to take a quick tour of what i do, or see the <Link href="/portfolio"><a>portfolio</a></Link> for a comprehensive overview.
       </Section>
 
       <Section height="100vh">
-        It all started with wordpress websites for local businesses, like a b and c.
+        <h1>Websites</h1>
+        The bread an butter of the operation are websites build using wordpress + page builder. The quick iteration time and large wordpress plugin ecosystem allow for efficient development. For examples, take a look <Link href="/portfolio?project=websites"><a>here</a></Link>.
       </Section>
 
       <Section height="100vh">
-        <h1>React</h1>
-        It all started with wordpress websites for local businesses, like a b and c.
+        <h1>Node + React</h1>
+        For custom products, my stack of choice is node + express for the backend, and react for anything frontend. The database largely depends on the project, i've worked with mySQL, postgreSQL, mongoDB and, unfortunately, MSSQL. I&apos;ve had the pleasure of developing some very interesting and complex applications with this stack, like <Link href="/portfolio?project=conditioner">this</Link> or <Link href="/portfolio?project=fenster_configurator">this</Link>.
       </Section>
 
       <Section height="100vh">
-        <h1>Simulation in Biology</h1>
-        Text unter der Überschrift lul
+        <h1>Simulation</h1>
+        For my masters degree, i&apos;ve developed a <Link href="http://localhost:3000/portfolio?project=sim">simulation model</Link> to investigate the evolution of some highly polymorphic immune genes (MHC, see background). To meet the performance requirements, it was implemented in c++, which i also enjoyed quite a bit. For the analysis, lots of stats and plotting was done in R and python.
       </Section>
 
 
