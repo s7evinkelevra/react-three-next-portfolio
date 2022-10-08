fa614442d11742b5d97a39a059e6166c9f0c0376
s7evinkelevra <kelevra.1337@gmail.com>
2022-10-08 02:06:21 +0200

    add content

--- src/components/dom/About.jsx
+++ src/components/dom/About.jsx
@@ -5,6 +5,7 @@ import profilePic from 'public/img/jan_comp.jpg'
 
 
 import styles from './About.module.css'
+import Link from 'next/link';
 
 const About = () => {
   return (
@@ -12,7 +13,18 @@ const About = () => {
       <div className={styles.box}>
         <div className={styles.text}>
           <h1>About Me</h1>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+          <p>
+            Hi, i&apos;m Jan Lüdemann. The best way to get to know me is to look at some of the <Link href="/portfolio">projects</Link> i did. If you don&apos;t want to do that, here is a quick summary: together with my brother, i&apos;ve been building for the web since 2018. This includes everything on the spectrum between designing and setting up wordpress sites for clients, consultation for online marketing to fullstack development. For most projects, i&apos;ve been the sole developer in the team, but also had the chance to collaborate with in-house developers of some clients.
+          </p>
+          <p>
+            For people who care about this sort of things, here is a list of technologies and methods i&apos;m familiar with or have used in the past: Javascript, R, Python, C++, mySQL, postgreSQL, mongoDB, SQLite, MSSQL, firebase, Node, Express, Wordpress, Woocommerce, React, NextJS, ThreeJS, Zustand, tailwindCSS, bootstrap, MaterialUI, radixUI, git, docker, AWS, Kanban, Agile, wet lab methods, DNA sequencing, population genetics analysis.
+          </p>
+          <p>
+            Recently, i&apos;ve also received my masters degree in biology (🎉), developing a simulation model.
+          </p>
+          <p>
+            Feel free to contact me at jan.luedemann@gmail.com
+          </p>
         </div>
       </div>
 
