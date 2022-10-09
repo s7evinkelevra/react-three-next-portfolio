309255f0ece4c20bf2adbdb007c3b393265db3f2
s7evinkelevra <kelevra.1337@gmail.com>
2022-10-09 18:50:44 +0200

    update content

--- src/components/dom/Overlay.jsx
+++ src/components/dom/Overlay.jsx
@@ -51,12 +51,12 @@ const Overlay = forwardRef(({ scroll }, ref) => {
 
       <Section height="100vh">
         <h1>Websites</h1>
-        At the core are websites build using wordpress + the divi page builder. The quick iteration time and large wordpress plugin ecosystem allow for efficient development. For examples, take a look <Link href="/portfolio?project=websites"><a>here</a></Link> or at the background :)
+        At the core of the business are websites build using wordpress + the divi page builder. The quick iteration time and large wordpress plugin ecosystem allow for efficient development. For examples, take a look <Link href="/portfolio?project=websites"><a>here</a></Link> or at the background :)
       </Section>
 
       <Section height="100vh">
         <h1>Node + React</h1>
-        For custom products, my stack of choice is node + express for the backend, and react for anything frontend. The database largely depends on the project, i&apos;ve worked with mySQL, postgreSQL, mongoDB and, unfortunately, MSSQL. I&apos;ve had the pleasure of developing some very interesting and complex applications with this stack, like <Link href="/portfolio?project=conditioner">this</Link> or <Link href="/portfolio?project=fenster_configurator">this</Link>.
+        For custom products, my stack of choice is node + express for the backend, and react for anything frontend. The database largely depends on the project, i&apos;ve worked with mySQL, postgreSQL, mongoDB and, unfortunately, MSSQL. I&apos;ve had the pleasure of developing some interesting and complex applications with this stack, like <Link href="/portfolio?project=conditioner">this</Link> or <Link href="/portfolio?project=fenster_configurator">this</Link>.
       </Section>
 
       <Section height="100vh">
